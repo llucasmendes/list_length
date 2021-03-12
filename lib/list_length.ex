8b65597e48dd8d3608ac67d8d276d2ea13069141
llucasmendes <llucasmendes@hotmail.com>
2021-03-12 18:52:04 -0300

    doc: documentation of the module

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,4 +1,8 @@
 defmodule ListLength do
+  @moduledoc """
+  Provides a function `call/1` to calc a list length
+  """
+
   defp lenght([], acc), do: acc
 
   defp lenght([_h | t], acc) do
@@ -6,5 +10,21 @@ defmodule ListLength do
     lenght(t, acc)
   end
 
+  @doc """
+  Calculate a list length
+
+  ## Parameters
+
+    - list: A list of any kind and lengh.
+
+  ## Examples
+
+      iex> ListLength.call([])
+      0
+
+      iex> ListLength.call([0, 1, 2, 3, 4])
+      5
+
+  """
   def call(list), do: lenght(list, 0)
 end
