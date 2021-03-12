c5d35098ba48af4ddef50cb688b930dd2b2f49c8
llucasmendes <llucasmendes@hotmail.com>
2021-03-12 19:17:19 -0300

    test: empty list test case

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,6 +1,11 @@
 defmodule ListLengthTest do
   use ExUnit.Case
 
+  test "test a empty list" do
+    list = []
+    assert ListLength.call(list) == length(list)
+  end
+
   test "test the list length" do
     list = [1, 2, 3, 4]
     assert ListLength.call(list) == length(list)
